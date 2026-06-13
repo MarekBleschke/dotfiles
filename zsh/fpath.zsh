346300af6acf077094054dc54c07a068c7a25843
MarekBleschke <marek.bleschke@gmail.com>
2026-06-13 06:19:59 +0200

    add podaman and completions to paht

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,3 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($MY_ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+fpath=($fpath "$HOME/.zsh/completions")
